8de749814db02c261a4643edcb72f15cb21928f6
SenDan07 <dany.alekrmz@gmail.com>
2022-09-14 02:36:40 -0700

    Set up project with Express

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const routes = require("./routes");
+const app = express();
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type ='text' name='title' /><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  // next() allows the response to continue to the next middleware in line
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
