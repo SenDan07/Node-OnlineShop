1d911445f476d8b2371794486a53acfa4a71bf81
SenDan07 <dany.alekrmz@gmail.com>
2022-09-17 13:54:30 -0700

    installed templating engines, created shop.pug view

--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require('../util/path')
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // next() allows the response to continue to the next middleware in line
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
