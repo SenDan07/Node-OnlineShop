f038e9bfd7f276585e068a2bf842aeafa4c7f85c
SenDan07 <dany.alekrmz@gmail.com>
2022-09-20 02:41:31 -0700

    price,description and image inputs addedto models and views

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
-const adminController = require("../controllers/shop");
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
 router.get("/add-product", adminController.getAddProduct);
 
-router.get("/products");
+router.get("/products", adminController.getProducts);
 
 router.post("/add-product", adminController.postAddProduct);
 
