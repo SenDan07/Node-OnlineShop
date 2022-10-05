ec2816c6dae98a925c6ff7fe67356408134e7188
SenDan07 <dany.alekrmz@gmail.com>
2022-10-05 00:35:01 -0700

    User model created

---
+++ models/user.js
@@ -0,0 +1,21 @@
+//id, name, email
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = User;
