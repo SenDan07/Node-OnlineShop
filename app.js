68c089459676f78a8ad0434d842af6041fe58ddf
SenDan07 <dany.alekrmz@gmail.com>
2022-11-15 00:13:20 -0800

    Login form and styles added

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -31,14 +32,15 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(`${DB_URL}`)
   .then((result) => {
-    User.findOne().then(user => {
-      if (!user){
+    User.findOne().then((user) => {
+      if (!user) {
         const user = new User({
           name: "Dan",
           email: "test@test.com",
@@ -48,7 +50,7 @@ mongoose
         });
         user.save();
       }
-    })
+    });
     console.log("Connected to mongoose DB");
     app.listen(3000);
   })
