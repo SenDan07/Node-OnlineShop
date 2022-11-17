9fba8f6039eb67a6a9c75ee817a4f558349e1d3f
SenDan07 <dany.alekrmz@gmail.com>
2022-11-16 17:38:55 -0800

    Styles updated, logout button created

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-const { DB_URL } = process.env;
+const { DB_URL, SESSION_URL } = process.env;
 
 const app = express();
+const store = new MongoDBStore({
+  uri: `${SESSION_URL}`,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -22,17 +27,13 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
-
-app.use((req, res, next) => {
-  User.findById("636cb5e1922c6388006de2f3")
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,25 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie");
-  console.log(req.get("Cookie"));
+  // const isLoggedIn = req.get("Cookie");
+
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  // res.setHeader("Set-Cookie", "loggedIn=true");
-  res.cookie("loggedIn", true, {
-    httpOnly: true,
-  });
-  res.redirect("/");
+  // res.cookie("loggedIn", true, {
+  //   httpOnly: true,
+  // });
+  User.findById("636cb5e1922c6388006de2f3")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
