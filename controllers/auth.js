a296e316759a73108cfda2bd0cd1a7434b55f2e8
SenDan07 <dany.alekrmz@gmail.com>
2023-01-19 16:37:32 -0600

    signup controller and email verification created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const user = require("../models/user");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
@@ -7,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
     isAuthenticated: false,
   });
 };
@@ -25,6 +34,30 @@ exports.postLogin = (req, res, next) => {
       });
     })
     .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect("/signup");
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
